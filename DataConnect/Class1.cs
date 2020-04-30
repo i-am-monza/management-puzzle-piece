b1d911b64bdcb8f92542f0c4041bb45554d4d65e
Monza Dingane <monwabisidingane@gmail.com>
2020-04-30 15:02:29 +0200

    Installed MySQL and integrated with project. Now we'll branch and work on testing if setups are in order.

---
+++ DataConnect/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace DataConnect
+{
+    public class Connect
+    {
+        Connect()
+        {
+            // assign a connection string to database
+            string connectionString = "server=localhost;database=Management;uid=root;";
+            // create connection object for opening connection
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            // just in case things break
+            try
+            {
+                // open connection
+                connection.Open();
+
+                Console.WriteLine("Connection Open!");
+            } 
+            catch(Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+        }
+    }
+}
