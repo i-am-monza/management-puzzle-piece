f4de136fc28ae43b0bf44fdc3de6c1a47e094b23
Monza Dingane <monwabisidingane@gmail.com>
2020-05-04 15:50:44 +0200

    Worked on defining the mothods this application will help. There's no logic yet, so test first.

--- DataConnect/Connect.cs
+++ DataConnect/Connect.cs
@@ -5,13 +5,15 @@ namespace DataConnect
 {
     public abstract class Connect
     {
+        // create connection object for opening connection
+        MySqlConnection connection;
+
         public void ConnectToDatabase()
         {
             // assign a connection string to database
             string connectionString = "server=localhost;user id=root;database=users";
-            // create connection object for opening connection
-            MySqlConnection connection = new MySqlConnection(connectionString);
-
+            // initialise connection object
+            connection = new MySqlConnection(connectionString);
             // just in case things break
             try
             {
@@ -24,8 +26,72 @@ namespace DataConnect
             {
                 Console.WriteLine("Error: {0}", e);
             }
+        }
 
-            connection.Close();
+        async public void CloseDatabaseConnection()
+        {
+            // just in case things break
+            try
+            {
+                //  conclosenection
+                await connection.CloseAsync();
+
+                Console.WriteLine("Connection Closed!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+        }
+
+        // method for inserting a new user
+        public bool InsertToDatabase(string userName, bool emplSTatus, string[] skills, DateTime regDate, string table = "users")
+        {
+            /*
+             * code here
+             */
+
+            return true;
+        }
+
+        // a method for viewing all users
+        public string[] ViewAllUsers(string table = "users")
+        {
+            /*
+             * code here
+             */
+
+            return new string[0];
+        }
+
+        // a method for searching with user id
+        public string[] SearchUser(string id, string table = "users")
+        {
+            /*
+             * code here
+             */
+
+            return new string[0];
+        }
+
+        // a method for updating user details
+        public bool UpdateUser(string id, string fieldForUpdate, string table = "users")
+        {
+            /*
+             * code here
+             */
+
+            return false;
+        }
+
+        // a method for deleting a user from records
+        public bool DeleteUser(string id, string table = "users")
+        {
+            /*
+             * code here
+             */
+
+            return false;
         }
     }
 }
--- Managers/Manage.cs
+++ Managers/Manage.cs
@@ -5,10 +5,14 @@ namespace Managers
 {
     public class Manage : Connect
     {
-
+        // method for connecting to the database
         public Manage()
         {
+            // connect to database
             ConnectToDatabase();
+
+            // close connection to database
+            CloseDatabaseConnection();
         }
     }
 }
